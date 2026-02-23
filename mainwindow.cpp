7923c790f0a54c2dc2b7f4dee40665ab605475db
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2026-02-23 18:31:27 +0900

    wip

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,103 @@
 #include "mainwindow.h"
+
+#include <QGraphicsItem>
+
+#include "./about.h"
 #include "./ui_mainwindow.h"
+
+class Item : public QGraphicsItem
+{
+public:
+    Item()
+        : QGraphicsItem()
+    {}
+    virtual ~Item() {}
+    QRectF boundingRect() const override { return QRectF(0, 0, 50, 10); }
+    void paint(QPainter *painter,
+               const QStyleOptionGraphicsItem *option,
+               QWidget *widget = nullptr) override
+    {
+        painter->setPen(QPen(this->color()));
+        painter->drawRect(0, 0, 50, 10);
+    }
+    virtual QColor color() = 0;
+};
+class FrontItem : public Item
+{
+public:
+    FrontItem()
+        : Item()
+    {}
+    ~FrontItem() {}
+    QColor color() override { return Qt::blue; }
+};
+class BackItem : public Item
+{
+public:
+    BackItem()
+        : Item()
+    {}
+    ~BackItem() {}
+    QColor color() override { return Qt::red; }
+};
+
+class Component
+{
+public:
+    Component(QGraphicsScene *frontScene, QGraphicsScene *backScene)
+        : front(new FrontItem())
+        , back(new BackItem())
+        , frontScene(frontScene)
+        , backScene(backScene)
+    {
+        frontScene->addItem(front);
+        backScene->addItem(back);
+    }
+    ~Component()
+    {
+        frontScene->removeItem(front);
+        backScene->removeItem(back);
+        delete front;
+        delete back;
+    }
+
+private:
+    FrontItem *front;
+    BackItem *back;
+    QGraphicsScene *frontScene;
+    QGraphicsScene *backScene;
+};
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , frontScene(new QGraphicsScene(this))
+    , backScene(new QGraphicsScene(this))
+    , dataList()
 {
     ui->setupUi(this);
+    frontScene->setSceneRect(0, 0, 400, 300);
+    backScene->setSceneRect(0, 0, 400, 300);
+    frontScene->addRect(0, 0, 400, 300);
+
+    auto c1 = new Component(frontScene, backScene);
+    dataList.push_front(c1);
+
+    ui->frontView->setScene(frontScene);
+    ui->backView->setScene(backScene);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    for (auto item : dataList) {
+        delete item;
+    }
+    dataList.clear();
+}
+
+void MainWindow::on_actionBoardes_triggered()
+{
+    auto about = About();
+    about.exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,17 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsScene>
+
+#include <forward_list>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
 QT_END_NAMESPACE
+
+class Component;
 
 class MainWindow : public QMainWindow
 {
@@ -17,7 +22,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionBoardes_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *frontScene;
+    QGraphicsScene *backScene;
+    std::forward_list<Component *> dataList;
 };
 #endif // MAINWINDOW_H
